9940f28a7211eb08e6cc1bb1868715b11073d8ff
cpheighter <cheighterjr@gmail.com>
2022-07-14 11:12:42 -0400

    init

---
+++ CarExercise/Program.cs
@@ -0,0 +1,7 @@
+﻿using CarExercise;
+
+var car1 = new Car("Dodge", "Viper", 2017);
+// Or Car car1 = new Car("Dodge", "Viper", 2017);
+Console.WriteLine($"{car1.Make} {car1.Model} {car1.Year}");
+
+
